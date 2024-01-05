d906c91b98a588d54c35c5bf8ed9a6d0e2a7d1b2
guillenicoud <132966306+guillenicoud@users.noreply.github.com>
2024-01-04 23:05:57 -0300

    20240104

--- TEST JS/functionalprogramming.js
+++ TEST JS/functionalprogramming.js
@@ -1,4 +1,21 @@
 // FUNCTIONAL PROGRAMMING
 
+
+// En programación funcional, las funciones se asignan a variables o pasarlas como parametros a otras funciones
+// 
+
 // Funciones de Orden Superior
+
+// Composicion: Permite tomar una funcion y pasarla como parametro a otra funcion de Ordern Superior
+// 
+// En Javascript se puede asignar a una variable una funcion anónima. Para asi reutilizarla. 
+
+var animals = [
+    { name: 'Fluffykins', species: 'rabbit' },
+    { name: 'Caro',       species: 'dog' },
+    { name: 'Hamilton',   species: 'dog' },
+    { name: 'Harold',     species: 'fish' },
+    { name: 'Ursula',     species: 'cat' },
+    { name: 'Jimmy',      species: 'fish' }
+]
 
