224545a2f95b39b31ca8f096b442a0f74f171744
guillenicoud <132966306+guillenicoud@users.noreply.github.com>
2024-01-09 13:34:25 -0300

    202319

--- TEST JS/functionalprogramming.js
+++ TEST JS/functionalprogramming.js
@@ -1,13 +1,12 @@
 // FUNCTIONAL PROGRAMMING
 
+// Las funciones de Alto Orden son funciones que toman como parametro otras funciones
 
-// En programación funcional, las funciones se asignan a variables o pasarlas como parametros a otras funciones
-// 
 
 // Funciones de Orden Superior
 
 // Composicion: Permite tomar una funcion y pasarla como parametro a otra funcion de Ordern Superior
-// 
+
 // En Javascript se puede asignar a una variable una funcion anónima. Para asi reutilizarla. 
 
 var animals = [
@@ -19,3 +18,40 @@ var animals = [
     { name: 'Jimmy',      species: 'fish' }
 ]
 
+// let dogs = [];
+
+// for (let i = 0; i < animals.length; i ++){
+//     if ( animals[i].species === 'dog' ){
+//         dogs.push(animals[i]);
+//     }
+// }
+// console.log(dogs);
+
+// FILTER: Es un metodo de un array, que toma otra funcion como argumento, y usa esa funcion para filtrar el array
+
+let isDog = function(animal){
+    return animal.species === 'dog'
+}
+
+let dogs = animals.filter(isDog)
+
+// console.log(dogs);
+
+// MAP: Es un metodo de  un array, que también toma una función como argumento pero a diferencia de filter, éste modifica el array
+
+let names = animals.map(function(animal){
+    return animal.name + ' is a ' + animal.species; 
+})
+
+console.log(names);
+
+// var orders = [
+//     { amount: 250 },
+//     { amount: 400 },
+//     { amount: 100 },
+//     { amount: 325 }
+// ]
+
+// var totalAmount = orders.reduce(function (sum, order) {
+//     return sum + order.amount
+// }, 0)
