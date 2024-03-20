95eede8b8dfc7ac73c682bdbd0bdcc9288465a79
guillenicoud <132966306+guillenicoud@users.noreply.github.com>
2024-03-19 23:55:34 -0300

    20240319

---
+++ freeCodeCamp/contador-de-clics/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freeCodeCampLogo.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo' 
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-principal'>
+    
+      </div>
+    </div>
+  );
+}
+
+export default App;
