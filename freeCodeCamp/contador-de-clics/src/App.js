e45a9e869aa1b3350cf361231f0618aac4a1a0ae
guillenicoud <132966306+guillenicoud@users.noreply.github.com>
2024-03-20 21:42:46 -0300

    2024320

--- freeCodeCamp/contador-de-clics/src/App.js
+++ freeCodeCamp/contador-de-clics/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import Boton from './componentes/Boton'
 import freeCodeCampLogo from './imagenes/freeCodeCampLogo.jpg'
 
+
 function App() {
+
+  const manejarClic = () => {
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () =>{
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -11,7 +22,14 @@ function App() {
           alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
-    
+        <Boton 
+          texto='Clic'
+          esBotondeClic={true}
+          manejarClic={manejarClic}/>
+        <Boton 
+          texto='Reiniciar'
+          esBotondeClick={false}
+          manejarClic={reiniciarContador}/>
       </div>
     </div>
   );
